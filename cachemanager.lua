142f46f66742f563d55609d5c3d1424f5e21fb0c
David Desbouis <github@desbouis.com>
2012-02-26 23:25:34 +0100

    use backend instead fallback in variables names

--- cachemanager.lua
+++ cachemanager.lua
@@ -22,46 +22,52 @@ local redis_read_response = ngx.location.capture("/redis_read", {args = {k = ngx
 local res, typ = parser.parse_reply(redis_read_response.body)
 
 if (typ == parser.BULK_REPLY and not(res == nil) and (#res > 0)) then
+    -- the content is in redis, just return the content with the good http code
     ngx.log(ngx.NOTICE, "[LUA] YES, cache HIT on cache key: ", ngx.var.key, ", content length: ", #res)
     ngx.print(res)
     ngx.exit(ngx.OK)
+
 else
     -- the content is not in redis, request to the backend
     ngx.log(ngx.NOTICE, "[LUA] NO, cache MISS on cache key: ", ngx.var.key)
-    local fallback_response = ngx.location.capture("/fallback"..ngx.var.key)
+    local backend_response = ngx.location.capture("/fallback"..ngx.var.key)
 
 -- @debug
---ngx.say("fallback response code :", fallback_response.status)
---ngx.say("fallback headers :")
---local h = fallback_response.header
+--ngx.say("backend response code :", backend_response.status)
+--ngx.say("backend headers :")
+--local h = backend_response.header
 --for k, v in pairs(h) do
 --    ngx.say(k..": "..v)
 --end
 --ngx.exit(ngx.HTTP_OK)
 
     -- write the response in redis
-    if fallback_response.status == ngx.HTTP_OK then
+    if backend_response.status == ngx.HTTP_OK then
+
         -- our header names declaration
         local ttl_header, expireat_header = "X-RedisCache-ttl", "X-RedisCache-expireat"
         -- by default, the data is persistent
         local expire_query = {"PERSIST", ngx.var.key}
         ngx.req.set_header("X-RedisCache-time", ngx.http_time(ngx.time()))
         ngx.log(ngx.NOTICE, "[LUA] got response from /fallback, uri ",ngx.var.key," must be cached")
-        -- get the fallback headers
+
+        -- get the backend headers
         local headers = {}
-        for k, v in pairs(fallback_response.header) do
+        for k, v in pairs(backend_response.header) do
             table.insert(headers, k..": "..v)
         end
+
         -- ttl or expiration date
-        if fallback_response.header[ttl_header] ~= nil then
-            expire_query = {"EXPIRE", ngx.var.key, fallback_response.header[ttl_header]}
-        elseif fallback_response.header[expireat_header] ~= nil then
-            expire_query = {"EXPIREAT", ngx.var.key, fallback_response.header[expireat_header]}
+        if backend_response.header[ttl_header] ~= nil then
+            expire_query = {"EXPIRE", ngx.var.key, backend_response.header[ttl_header]}
+        elseif backend_response.header[expireat_header] ~= nil then
+            expire_query = {"EXPIREAT", ngx.var.key, backend_response.header[expireat_header]}
         end
+
         -- redis transaction
         local queries = {
             {"MULTI"},
-            {"HMSET", ngx.var.key, "headers", table.concat(headers, "\\n"), "body", fallback_response.body},
+            {"HMSET", ngx.var.key, "headers", table.concat(headers, "\\n"), "body", backend_response.body},
             expire_query,
             {"EXEC"}
         }
@@ -70,6 +76,8 @@ else
         for i, query in ipairs(queries) do
             table.insert(raw_queries, parser.build_query(query))
         end
+
+        -- let's write now
         local queries_response = ngx.location.capture("/redis_write?n="..#queries, { method = ngx.HTTP_POST, body = table.concat(raw_queries, "") })
         if queries_response.status ~= 200 or not queries_response.body then
             ngx.log(ngx.ERR, "[LUA] failed to query redis to store data")
@@ -84,7 +92,7 @@ else
 --ngx.exit(ngx.HTTP_OK)
 
         -- all is good, we return the content with 200 status code
-        ngx.print(fallback_response.body)
+        ngx.print(backend_response.body)
         ngx.exit(ngx.OK)
     end
 end
